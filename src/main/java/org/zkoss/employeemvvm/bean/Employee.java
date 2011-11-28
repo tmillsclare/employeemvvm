df9fc4fc6dabd835b7e1485e9a37fc272a77ce4c
Timothy Clare <timothyclare@potix.com>
2011-11-28 18:57:34 +0800

    Completed the ZK 6 MVVM example

---
+++ src/main/java/org/zkoss/employeemvvm/bean/Employee.java
@@ -0,0 +1,80 @@
+package org.zkoss.employeemvvm.bean;
+
+import java.util.UUID;
+
+public class Employee {
+	private String _uuid, _firstName, _lastName;
+	private int _age;
+	
+	
+	private Employee(String uuid, String firstName, String lastName, int age) {
+		_uuid = uuid;
+		_firstName = firstName;
+		_lastName = lastName;
+		_age = age;
+	}
+	
+	public Employee(String firstName, String lastName, int age) {
+		this(UUID.randomUUID().toString(), firstName, lastName, age);
+	}
+
+	public String getId() {
+		return _uuid;
+	}
+
+	public String getFirstName() {
+		return _firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this._firstName = firstName;
+	}
+
+	public String getLastName() {
+		return _lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this._lastName = lastName;
+	}
+
+	public int getAge() {
+		return _age;
+	}
+	
+	public void setAge(int age) {
+		this._age = age;
+	}
+	
+	public Employee copy() {
+		return new Employee(this._uuid, this._firstName, this._lastName, this._age);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Employee)) return false;
+		
+		Employee employee = (Employee)obj;
+		return this._uuid.equals(employee.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return _uuid.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("Employee {uuid = ");
+		sb.append(getId());
+		sb.append(", firstName = ");
+		sb.append(getFirstName());
+		sb.append(", lastName = ");
+		sb.append(getLastName());
+		sb.append(", age = ");
+		sb.append(getAge());
+		sb.append("}");
+		
+		return sb.toString();
+	}
+}
---
+++ src/main/java/org/zkoss/employeemvvm/viewmodel/EmployeeViewModel.java
@@ -0,0 +1,101 @@
+package org.zkoss.employeemvvm.viewmodel;
+
+import org.zkoss.bind.NotifyChange;
+import org.zkoss.employeemvvm.bean.Employee;
+import org.zkoss.employeemvvm.service.EmployeeService;
+import org.zkoss.employeemvvm.utils.Messages;
+import org.zkoss.util.logging.Log;
+import org.zkoss.zul.ListModel;
+import org.zkoss.zul.ListModelList;
+
+public class EmployeeViewModel {
+
+	private static final Log log = Log.lookup(EmployeeViewModel.class);
+	
+	private Employee _selected;
+	private ListModelList<Employee> _model = EmployeeService.INSTANCE.getModel();
+	
+	private String _error = null;
+	
+	public String getError() {
+		return _error;
+	}
+	
+	@NotifyChange("error")
+	public void setError(String error) {
+		_error = error;
+	}
+	
+	public Employee getSelected() {
+		return _selected;
+	}
+	
+	
+	@NotifyChange("selected")
+	public void setSelected(Employee selected) {		
+		if(selected != null) {
+			this._selected = selected.copy();
+		} else {
+			selected = null;
+		}
+	}
+
+	public ListModel<Employee> getEmployees() {
+		return _model;
+	}
+	
+	@NotifyChange({"employees", "selected"})
+	public void add() {
+		Employee employee = new Employee(_selected.getFirstName(), _selected.getLastName(), _selected.getAge());
+		_model.add(employee);
+		setSelected(employee);
+	}
+	
+	@NotifyChange({"employees", "error", "selected"})
+	public void update() {
+		boolean success = false;
+		
+		if (_selected != null) {
+			int index = _model.indexOf(_selected);
+			
+			if(index >= 0) {
+				Employee employee = _model.get(index);
+				employee.setAge(_selected.getAge());
+				employee.setFirstName(_selected.getFirstName());
+				employee.setLastName(_selected.getLastName());
+				success = true;
+				setError(null);
+			}
+		}
+	
+		if(!success) {
+			String error;
+			setError(error = Messages.getString("EmployeeController.2"));
+			log.error(error);
+		}
+	
+	}
+	
+	@NotifyChange({"employees", "error", "selected"})
+	public void remove() {
+
+		boolean success = false;
+		
+		if (_selected != null) {
+			int index = _model.indexOf(_selected);
+			if(index >= 0) {
+				_model.remove(index);
+				setSelected(_selected);
+				success = true;
+				setError(null);
+			}
+		}
+	
+		if(!success) {
+			String error;
+			setError(error = Messages.getString("EmployeeController.4"));
+			log.error(error);
+		}
+
+	}
+}
